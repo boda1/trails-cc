2eb8bcf99aa52da169d1539e9a21308e2ea1d51c
Adam Boda <adam.boda@MBP-ADAMBODA.local>
2022-07-07 07:56:19 +0100

    added placeholder images to route cards

--- components/header.js
+++ components/header.js
@@ -5,15 +5,21 @@ import styles from '../styles/header.module.css'
 export default function Header() {
     return (
         <header>
-           <div classname={styles.siteTitleContainer}>
-                <Image
-                    className={styles.siteTitleBackground}
-                    alt="title background image"
-                    src="/king-alfred-preview.jpeg"
-                    width={1200}
-                    height={400}
-                />
-                <div className={styles.siteTitle}>Trails.cc</div>
-            </div> 
+                <div className={styles.siteTitle}>
+                    Trails.cc
+                </div>
+                {/*  
+
+                not ready to apply styling to next.js image component 
+                
+                <div className={styles.siteTitleBackground}>
+                    <Image
+                        alt="title background image"
+                        src="/king-alfred-preview.jpeg"
+                        width={100}
+                        height={400}
+                    />
+                </div>
+                */}
         </header>
     )}
